6a9b9e33274f2cc77d5ba0a2459bd3ad5fb8470d
JAMES ASIBEY MANU <jaamanu@gmail.com>
2022-09-28 00:02:57 +0200

    fix erros

--- jvs.js
+++ jvs.js
@@ -1,5 +1,6 @@
-import Store from "./modules/store.js";
-import UI from "./modules/ui.js";
+import Store from './modules/store.js';
+import UI from './modules/ui.js';
+
 class Book {
   constructor(title, author, id = Math.floor(Math.random() * 1000000000000000)) {
     this.title = title;
@@ -10,58 +11,25 @@ class Book {
 
 document.addEventListener('DOMContentLoaded', UI.showBooks);
 document.querySelector('#book-card').addEventListener('submit', (e) => {
-  e.preventDefault()
-  console.log('hey')
-  const title = document.querySelector('#title').value
-  const author = document.querySelector('#author').value
-if(title !== '' && author !== '') {
-  const book = new Book(title, author)
-  UI.addBookToList(book)
-  UI.clearFields()
-  Store.addBooks(book)
-}
-})
+  e.preventDefault();
+  // console.log('hey');
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  if (title !== '' && author !== '') {
+    const book = new Book(title, author);
+    UI.addBookToList(book);
+    UI.clearFields();
+    Store.addBooks(book);
+  }
+});
 
 document.querySelector('.bookDisplay').addEventListener('click', (e) => {
-  if(e.target.className === 'remove') {
+  if (e.target.className === 'remove') {
     const id = e.target.previousElementSibling.innerText;
-    Store.deleteBookStorage(id)
-    UI.deleteBook(e.target)
+    Store.deleteBookStorage(id);
+    UI.deleteBook(e.target);
   }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 // const title = document.querySelector('#title');
 // const author = document.querySelector('#author');
